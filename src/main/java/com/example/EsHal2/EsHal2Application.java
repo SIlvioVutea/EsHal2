a2dcb755d12faff35bb60e1a082115920ec661d5
SIlvioVutea <silviovutea@gmail.com>
2024-06-26 18:46:58 +0200

    done

---
+++ src/main/java/com/example/EsHal2/EsHal2Application.java
@@ -0,0 +1,13 @@
+package com.example.EsHal2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsHal2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsHal2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EsHal2/EsHal2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EsHal2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsHal2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
